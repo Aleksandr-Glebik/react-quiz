b770e113aa9aa9496c526ec7a5129b8b7cda7542
Aleksandr-Glebik <glebikaa@mail.ru>
2022-07-27 17:23:35 +0300

    feat: add global store for Quiz comp, add reducer and action for Quiz

--- react-quiz/src/containers/Quiz/Quiz.js
+++ react-quiz/src/containers/Quiz/Quiz.js
@@ -2,19 +2,12 @@ import React, {Component} from "react"
 import classes from './Quiz.module.css'
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz"
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz"
-import axios from '../../axios/axios-quiz'
 import Loader from "../../components/UI/Loader/Loader"
 import { useLocation, useNavigate, useParams } from "react-router-dom"
+import { connect } from "react-redux"
+import {fetchQuizById} from '../../store/actions/quiz'
 
 class Quiz extends Component {
-    state = {
-        results: {}, // { [id]: 'success' or 'err' }
-        isFinished: false,
-        activeQuestion: 0,
-        answerState: null, // { [id]: 'success' or 'err' }
-        quiz: [],
-        loading: true,
-    }
 
     onAnswerClickHandler = (answerId) => {
         // console.log('answer id', answerId);
@@ -78,19 +71,10 @@ class Quiz extends Component {
         })
     }
 
-    async componentDidMount() {
-        try {
-            const response = await axios.get(`quizes/${this.props.router.params.id}.json`)
-            const quiz = response.data
-
-            this.setState({
-                quiz,
-                loading: false
-            })
-
-        } catch (error) {
-            console.log('error', error);
-        }
+    componentDidMount() {
+        console.log('this.props', this.props);
+        console.log('this.props.router.params.id', this.props.router.params.id);
+        this.props.fetchQuizById(this.props.router.params.id)
     }
 
     render() {
@@ -98,22 +82,22 @@ class Quiz extends Component {
             <div className={classes.Quiz}>
                 <h1>Ответьте на все вопросы</h1>
 
-                { this.state.loading
+                { this.props.loading || !this.props.quiz
                     ? <Loader />
-                    : this.state.isFinished
+                    : this.props.isFinished
                         ?  <FinishedQuiz
-                            results={this.state.results}
-                            quiz={this.state.quiz}
+                            results={this.props.results}
+                            quiz={this.props.quiz}
                             onRetry={this.retryHandler}
                         />
                         : <div className={classes.QuizWrapper}>
                             <ActiveQuiz
-                                answers={this.state.quiz[this.state.activeQuestion].answers}
-                                question={this.state.quiz[this.state.activeQuestion].question}
+                                answers={this.props.quiz[this.props.activeQuestion].answers}
+                                question={this.props.quiz[this.props.activeQuestion].question}
                                 onAnswerClick={this.onAnswerClickHandler}
-                                quizLength={this.state.quiz.length}
-                                answerNumber={this.state.activeQuestion + 1}
-                                state={this.state.answerState}
+                                quizLength={this.props.quiz.length}
+                                answerNumber={this.props.activeQuestion + 1}
+                                state={this.props.answerState}
                             />
                         </div>
                 }
@@ -134,7 +118,26 @@ function withRouter(Component) {
     }
 
     return ComponentWithRouterProp
-  }
+}
 
-export default withRouter(Quiz)
-// export default Quiz
+function mapStateToProps(state) {
+
+    return {
+        results: state.quiz.results,
+        isFinished: state.quiz.isFinished,
+        activeQuestion: state.quiz.activeQuestion,
+        answerState: state.quiz.answerState,
+        quiz: state.quiz.quiz,
+        loading: state.quiz.loading,
+    }
+
+}
+
+function mapDispatchToProps(dispatch) {
+    return  {
+        fetchQuizById: id => dispatch(fetchQuizById(id))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Quiz))
+
